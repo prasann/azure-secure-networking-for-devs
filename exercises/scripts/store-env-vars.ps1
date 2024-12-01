8eaee7333509cbfeea9ef8121c7097854a0528f6
Prasanna Nagarajan <prasanna.nagarajan@microsoft.com>
2024-12-01 17:00:14 +0530

    storing the env vars into a file for later usage

--- exercises/scripts/store-env-vars.ps1
+++ exercises/scripts/store-env-vars.ps1
@@ -1,24 +1,29 @@
 # Store-EnvVars.ps1
 
-# Capture all current environment variables
-$EnvVars = Get-ChildItem Env: | ForEach-Object {
-    [PSCustomObject]@{ Name = $_.Name; Value = $_.Value }
-}
+param(
+    [Parameter(Mandatory=$true)]
+    [hashtable]$EnvVarsToStore
+)
 
 # Convert the environment variables to a hashtable
 $EnvVarsHashtable = @{}
-foreach ($envVar in $EnvVars) {
-    $EnvVarsHashtable[$envVar.Name] = $envVar.Value
+foreach ($key in $EnvVarsToStore.Keys) {
+    $EnvVarsHashtable[$key] = $EnvVarsToStore[$key]
 }
 
 # Check if the env-vars.json file exists
-if (Test-Path -Path "env.json") {
-    # Load existing environment variables from the file
-    $existingEnvVars = Get-Content -Raw -Path "env.json" | ConvertFrom-Json
-    # Merge new environment variables with existing ones
-    $mergedEnvVars = [ordered]@{}
-    $existingEnvVars.PSObject.Properties.Name | ForEach-Object { $mergedEnvVars[$_] = $existingEnvVars.$_ }
-    $EnvVarsHashtable.Keys | ForEach-Object { $mergedEnvVars[$_] = $EnvVarsHashtable[$_] }
+if (Test-Path -Path "env.json" -PathType Leaf -ErrorAction SilentlyContinue) {
+    $fileContent = Get-Content -Raw -Path "env.json"
+    if ($fileContent -ne "") {
+        # Load existing environment variables from the file
+        $existingEnvVars = $fileContent | ConvertFrom-Json
+        # Merge new environment variables with existing ones
+        $mergedEnvVars = [ordered]@{}
+        $existingEnvVars.PSObject.Properties.Name | ForEach-Object { $mergedEnvVars[$_] = $existingEnvVars.$_ }
+        $EnvVarsHashtable.Keys | ForEach-Object { $mergedEnvVars[$_] = $EnvVarsHashtable[$_] }
+    } else {
+        $mergedEnvVars = $EnvVarsHashtable
+    }
 } else {
     $mergedEnvVars = $EnvVarsHashtable
 }
