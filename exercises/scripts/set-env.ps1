73c138bc31c3fe6ce9fadebba360732b361af8ea
Prasanna Nagarajan <prasanna.nagarajan@microsoft.com>
2026-08-25 17:40:28 +0530

    revert us region fallback

--- exercises/scripts/set-env.ps1
+++ exercises/scripts/set-env.ps1
@@ -3,7 +3,7 @@
 param(
     [string]$TeamName = "newteam",
     [string]$EuLocation = "westeurope",
-    [string]$UsLocation = "westus2",
+    [string]$UsLocation = "eastus2",
     [string]$HubLocation = "swedencentral"
 )
 
